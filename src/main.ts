0b7b823994649feeef91bb737e9cb5f7e4d8e6ce
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-06 19:44:57 -0300

    add swagger package

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Store Api')
+    .setVersion('1.0')
+    .setDescription('API that simulate a store, developed to learn NestJS. ')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
