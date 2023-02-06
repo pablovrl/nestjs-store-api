2292fe9ad6d800abaee33c026fcd992b511689e4
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-06 18:47:33 -0300

    add category module to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [ProductsModule, PrismaModule],
+  imports: [ProductsModule, PrismaModule, CategoriesModule],
   controllers: [],
   providers: [],
 })
