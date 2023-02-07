a91053cd7ca0096451a2bf1a60143d92c0e0a950
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-06 23:21:59 -0300

    use port env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule, PrismaModule, CategoriesModule, AuthModule],
+  imports: [
+    ProductsModule,
+    PrismaModule,
+    CategoriesModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  static port: string;
+  constructor(configService: ConfigService) {
+    AppModule.port = configService.get('PORT');
+  }
+}
