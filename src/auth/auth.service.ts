c2ae58ac911dad65f0fe55480559afd34bc65cf0
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-09 15:59:46 -0300

    add jwt validation strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,16 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SignInDto, SignUpDto } from './dto';
 import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: SignUpDto) {
     const hashedPassword = await argon2.hash(dto.password);
@@ -21,9 +27,8 @@ export class AuthService {
           },
         },
       });
-      delete user.password;
 
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (e) {
       if (e.code === 'P2002')
         throw new ForbiddenException('Credenntials taken');
@@ -42,8 +47,22 @@ export class AuthService {
     const passwordMatches = await argon2.verify(user.password, dto.password);
     if (!passwordMatches) throw new ForbiddenException('Invalid credentials');
 
-    delete user.password;
+    return this.signToken(user.id, user.email);
+  }
 
-    return user;
+  signToken(userId: number, email: string) {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const access_token = this.jwt.sign(payload, {
+      expiresIn: '1d',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    return {
+      access_token,
+    };
   }
 }
