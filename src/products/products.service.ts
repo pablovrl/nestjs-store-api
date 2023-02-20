39a4449bb5962de8732978b739e093344f6b7b18
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-20 01:08:44 -0300

    fix prisma import

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { UpdateProductDto } from './dto';
 import { CreateProductDto } from './dto/create-product.dto';
 
