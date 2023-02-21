23bd86ef2ddb466193fa28a87afa57489b569d71
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-21 11:40:49 -0300

    fix prisma import

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { SignInDto, SignUpDto } from './dto';
 import * as argon2 from 'argon2';
 import { JwtService } from '@nestjs/jwt';
